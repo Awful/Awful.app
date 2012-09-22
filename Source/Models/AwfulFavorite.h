8689e0d95054b6ac4e7c1ec1aa8faa0ea74d5d61
Nolan Waite <nolan@nolanw.ca>
2012-09-22 00:25:28 -0600

    Finish removing Favorite model.

--- Source/Models/AwfulFavorite.h
+++
@@ -1,5 +0,0 @@
-#import "_AwfulFavorite.h"
-
-@interface AwfulFavorite : _AwfulFavorite {}
-// Custom logic goes here.
-@end
--- Source/Models/AwfulModels.h
+++ Source/Models/AwfulModels.h
@@ -1,4 +1,3 @@
-#import "AwfulFavorite.h"
 #import "AwfulForum.h"
 #import "AwfulThread.h"
 #import "AwfulUser.h"
