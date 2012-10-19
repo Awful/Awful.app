4971aa87a6e84e8c4a9c2d0248208384ba12581c
Nolan Waite <nolan@nolanw.ca>
2012-10-19 12:10:54 -0300

    Fix thread actions in iPhone landscape.
    Action sheets don't like entirely covering the view they're presented from.
    Instead of presenting from the AwfulPage view, present from its superview,
    which won't get clipped by the action sheet.

--- Source/Posts/AwfulPage.m
+++ Source/Posts/AwfulPage.m
@@ -424,7 +424,9 @@
 {
     self.actions = [[AwfulThreadActions alloc] initWithThread:self.thread];
     self.actions.viewController = self;
-    [self.actions showFromRect:self.pageBar.frame inView:self.view animated:YES];
+    [self.actions showFromRect:[self.view convertRect:self.pageBar.frame toView:self.view.superview]
+                        inView:self.view.superview
+                      animated:YES];
 }
 
 - (void)tappedPageNav:(id)sender
