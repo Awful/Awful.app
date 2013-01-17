51d797d6be723aa3186f70f7ef9541cee7b5495c
Nolan Waite <nolan@nolanw.ca>
2013-01-17 16:47:54 -0400

    Nicer-looking extra keyboard keys for replies.
    Extracted the extra keys into their own classes and made them look a bit nicer.
    
    I can't tell if it's good that they look more like the actual keys, or bad
    because the reasons they aren't actual keys stand out more. More polish needed.
    
    This is good enough for now though. Closes #131

---
+++ Source/Posts/AwfulKeyboardBar.m
@@ -0,0 +1,120 @@
+//
+//  AwfulKeyboardBar.m
+//  Awful
+//
+//  Created by Nolan Waite on 2013-01-17.
+//  Copyright (c) 2013 Regular Berry Software LLC. All rights reserved.
+//
+
+#import "AwfulKeyboardBar.h"
+
+@interface AwfulKeyboardButton : UIButton
+
+@property (readonly, copy, nonatomic) NSString *character;
+
+- (instancetype)initWithCharacter:(NSString *)character;
+
+@end
+
+
+@interface AwfulKeyboardBar ()
+
+@property (weak, nonatomic) CAGradientLayer *gradient;
+
+@end
+
+
+@implementation AwfulKeyboardBar
+
+- (void)setCharacters:(NSArray *)characters
+{
+    if ([_characters isEqualToArray:characters]) return;
+    _characters = [characters copy];
+    [self.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    for (NSString *character in characters) {
+        AwfulKeyboardButton *button = [[AwfulKeyboardButton alloc] initWithCharacter:character];
+        [button addTarget:self action:@selector(keyPressed:)
+         forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:button];
+    }
+    [self setNeedsLayout];
+}
+
+- (void)keyPressed:(AwfulKeyboardButton *)button
+{
+    [[UIDevice currentDevice] playInputClick];
+    [self.keyInputView insertText:button.character];
+}
+
+#pragma mark - UIView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if (!(self = [super initWithFrame:frame])) return nil;
+    self.backgroundColor = [UIColor clearColor];
+    self.opaque = NO;
+    CAGradientLayer *gradient = [CAGradientLayer new];
+    gradient.colors = @[
+        (id)[UIColor colorWithWhite:0 alpha:0.6].CGColor,
+        (id)[UIColor colorWithWhite:0 alpha:0.7].CGColor
+    ];
+    [self.layer insertSublayer:gradient atIndex:0];
+    _gradient = gradient;
+    self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    self.gradient.frame = (CGRect){ .size = self.bounds.size };
+    const CGFloat width = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? 60 : 44;
+    const CGFloat between = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? 10 : 5;
+    CGFloat x = floorf((CGRectGetWidth(self.bounds) -
+                        (width * [self.characters count]) -
+                        (between * ([self.characters count] - 1)))
+                       / 2);
+    for (NSUInteger i = 0; i < [self.characters count]; i++) {
+        [self.subviews[i] setFrame:CGRectMake(x, 2, width, CGRectGetHeight(self.bounds) - 4)];
+        x += width + between;
+    }
+}
+
+#pragma mark - UIInputViewAudioFeedback
+
+- (BOOL)enableInputClicksWhenVisible
+{
+    return YES;
+}
+
+@end
+
+
+@implementation AwfulKeyboardButton
+{
+    NSString *_character;
+}
+
+- (id)initWithCharacter:(NSString *)character
+{
+    if (!(self = [super init])) return nil;
+    _character = [character copy];
+    [self setTitle:_character forState:UIControlStateNormal];
+    [self setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self setTitleShadowColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    
+    self.titleLabel.font = [UIFont boldSystemFontOfSize:22];
+    self.titleLabel.shadowOffset = CGSizeMake(0, 1);
+    
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        self.backgroundColor = [UIColor colorWithHue:0.667 saturation:0.019 brightness:0.827
+                                               alpha:1];
+    } else {
+        self.backgroundColor = [UIColor colorWithWhite:0.96 alpha:1];
+    }
+    self.layer.cornerRadius = 5;
+    self.layer.borderColor = [UIColor blackColor].CGColor;
+    self.layer.borderWidth = 1;
+    return self;
+}
+
+@end
--- Source/Posts/AwfulReplyViewController.m
+++ Source/Posts/AwfulReplyViewController.m
@@ -9,6 +9,7 @@
 #import "AwfulReplyViewController.h"
 #import "AwfulAlertView.h"
 #import "AwfulHTTPClient.h"
+#import "AwfulKeyboardBar.h"
 #import "AwfulModels.h"
 #import "AwfulSettings.h"
 #import "AwfulTheme.h"
@@ -55,14 +56,8 @@
 
 @end
 
+
 @implementation AwfulReplyViewController
-{
-    UIBarButtonItem *_insertOpenBracketButton;
-    UIBarButtonItem *_insertCloseBracketButton;
-    UIBarButtonItem *_insertEqualsButton;
-    UIBarButtonItem *_insertSlashButton;
-    UIBarButtonItem *_insertColonButton;
-}
 
 - (void)dealloc
 {
@@ -307,33 +302,6 @@ withImagePlaceholderResults:placeholderResults
     [self retheme];
 }
 
-#define ImplementInsertCharacterButton(PROPERTY, CHARACTER) \
-- (UIBarButtonItem *)insert ## PROPERTY ## Button \
-{ \
-    if (_insert ## PROPERTY ## Button) return _insert ## PROPERTY ## Button; \
-    _insert ## PROPERTY ## Button = [[UIBarButtonItem alloc] initWithTitle:CHARACTER \
-                                                             style:UIBarButtonItemStyleBordered \
-                                                            target:self \
-                                                            action:@selector(insertCharacter:)]; \
-    _insert ## PROPERTY ## Button.width = 44; \
-    return _insert ## PROPERTY ## Button; \
-}
-
-ImplementInsertCharacterButton(OpenBracket, @"[")
-
-ImplementInsertCharacterButton(CloseBracket, @"]")
-
-ImplementInsertCharacterButton(Equals, @"=")
-
-ImplementInsertCharacterButton(Slash, @"/")
-
-ImplementInsertCharacterButton(Colon, @":")
-
-- (void)insertCharacter:(UIBarButtonItem *)buttonItem
-{
-    [self.replyTextView insertText:buttonItem.title];
-}
-
 #pragma mark - Menu items
 
 - (void)configureTopLevelMenuItems
@@ -508,27 +476,14 @@ static UIImagePickerController *ImagePickerForSourceType(NSInteger sourceType)
 
 - (void)loadView
 {
-    UITextView *textView = [UITextView new];
+    UITextView *textView = [[UITextView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
     textView.font = [UIFont systemFontOfSize:17];
-    UIToolbar *bbCodeBar = [UIToolbar new];
-    bbCodeBar.frame = CGRectMake(0, 0, 0, 44);
-    bbCodeBar.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    bbCodeBar.barStyle = UIBarStyleBlack;
-    bbCodeBar.translucent = YES;
-    UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
-                                                                           target:nil action:NULL];
-    UIBarButtonItem *anotherSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
-                                                                                  target:nil action:NULL];
-    bbCodeBar.items = @[
-        self.insertOpenBracketButton,
-        self.insertEqualsButton,
-        space,
-        self.insertColonButton,
-        anotherSpace,
-        self.insertSlashButton,
-        self.insertCloseBracketButton
-    ];
-    textView.inputAccessoryView = bbCodeBar;
+    AwfulKeyboardBar *bbcodeBar = [[AwfulKeyboardBar alloc] initWithFrame:
+                                   CGRectMake(0, 0, CGRectGetWidth(textView.bounds),
+                                              UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? 63 : 36)];
+    bbcodeBar.characters = @[ @"[", @"=", @":", @"/", @"]" ];
+    bbcodeBar.keyInputView = textView;
+    textView.inputAccessoryView = bbcodeBar;
     self.view = textView;
     [PSMenuItem installMenuHandlerForObject:self.view];
 }
