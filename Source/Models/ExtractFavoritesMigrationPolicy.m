4ebec4ae4d9f0480166cdbb0602e747d8dd1ca3f
Nolan Waite <nolan@nolanw.ca>
2013-03-08 02:40:34 -0400

    Migrating favorites relies less on AwfulSettings.
    We can use the migration manager's userInfo dictionary instead.
    
    Originally I'd tried to store an ivar in the migration policy, but new
    instances of the policy get created as the migration continues.

--- Source/Models/ExtractFavoritesMigrationPolicy.m
+++ Source/Models/ExtractFavoritesMigrationPolicy.m
@@ -11,28 +11,32 @@
 
 @implementation ExtractFavoritesMigrationPolicy
 
-- (BOOL)beginEntityMapping:(NSEntityMapping *)mapping manager:(NSMigrationManager *)manager
-                     error:(NSError *__autoreleasing *)error
-{
-    [AwfulSettings settings].favoriteForums = nil;
-    return [super beginEntityMapping:mapping manager:manager error:error];
-}
-
 - (BOOL)createDestinationInstancesForSourceInstance:(NSManagedObject *)sInstance
                                       entityMapping:(NSEntityMapping *)mapping
                                             manager:(NSMigrationManager *)manager
                                               error:(NSError *__autoreleasing *)error
 {
+    // The isFavorite attribute is disappearing, so we want to extract the IDs of each favorited
+    // forum for later storage in AwfulSettings.
     if ([[sInstance valueForKey:@"isFavorite"] boolValue]) {
-        NSMutableArray *favorites = [[AwfulSettings settings].favoriteForums mutableCopy];
-        if (!favorites) favorites = [NSMutableArray new];
+        // We'll store the forum IDs in the migration manager's userInfo dictionary.
+        if (!manager.userInfo) {
+            manager.userInfo = [NSMutableDictionary new];
+        }
+        NSMutableDictionary *userInfo = (id)manager.userInfo;
+        if (!userInfo[@"favorites"]) {
+            userInfo[@"favorites"] = [NSMutableArray new];
+        }
+        NSMutableArray *favorites = userInfo[@"favorites"];
+        // We get the AwfulForum instances in an arbitrary order, so fill up the array with dummy
+        // values we can remove later.
         NSInteger targetIndex = [[sInstance valueForKey:@"favoriteIndex"] integerValue];
         while ((NSInteger)[favorites count] <= targetIndex) {
             [favorites addObject:@(NSNotFound)];
         }
+        // Then replace the dummy value with the actual forum ID.
         [favorites replaceObjectAtIndex:targetIndex
                              withObject:[sInstance valueForKey:@"forumID"]];
-        [AwfulSettings settings].favoriteForums = favorites;
     }
     return [super createDestinationInstancesForSourceInstance:sInstance entityMapping:mapping
                                                       manager:manager error:error];
@@ -41,7 +45,9 @@
 - (BOOL)endEntityMapping:(NSEntityMapping *)mapping manager:(NSMigrationManager *)manager
                    error:(NSError *__autoreleasing *)error
 {
-    NSMutableArray *favorites = [[AwfulSettings settings].favoriteForums mutableCopy];
+    // Now that we're done migrating AwfulForums, we can save our array of forum IDs to
+    // AwfulSettings.
+    NSMutableArray *favorites = (id)manager.userInfo[@"favorites"];
     [favorites removeObjectIdenticalTo:@(NSNotFound)];
     [AwfulSettings settings].favoriteForums = favorites;
     return [super endEntityMapping:mapping manager:manager error:error];
