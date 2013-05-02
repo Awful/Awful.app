ecbb68918346c7eb8cc68b539427db2c87787a43
Nolan Waite <nolan@nolanw.ca>
2013-05-02 18:08:18 -0300

    Set page bottom bar appearance once.

--- Source/Posts/AwfulPageBottomBar.m
+++ Source/Posts/AwfulPageBottomBar.m
@@ -9,66 +9,38 @@
 
 @implementation AwfulPageBottomBar
 
+#pragma mark - UIView
+
 - (id)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        self.autoresizingMask = (UIViewAutoresizingFlexibleWidth |
-                                 UIViewAutoresizingFlexibleTopMargin);
-        
-        UIImage *previousPage = [UIImage imageNamed:@"arrowleft.png"];
-        previousPage.accessibilityLabel = @"Previous page";
-        UIImage *nextPage = [UIImage imageNamed:@"arrowright.png"];
-        nextPage.accessibilityLabel = @"Next page";
-        UISegmentedControl *backForwardControl = [[UISegmentedControl alloc]
-                                                  initWithItems:@[ previousPage, nextPage ]];
-        [self addSubview:backForwardControl];
-        _backForwardControl = backForwardControl;
-        
-        UIButton *jumpToPage = [UIButton buttonWithType:UIButtonTypeCustom];
-        jumpToPage.titleLabel.font = [UIFont boldSystemFontOfSize:11];
-        jumpToPage.accessibilityHint = @"Jumps to a page";
-        [self addSubview:jumpToPage];
-        _jumpToPageButton = jumpToPage;
-        
-        UIImage *action = [UIImage imageNamed:@"action.png"];
-        action.accessibilityLabel = @"Thread actions";
-        UIImage *fontSize = [UIImage imageNamed:@"font-size.png"];
-        fontSize.accessibilityLabel = @"Adjust style";
-        UISegmentedControl *actionsFontSize = [[UISegmentedControl alloc]
-                                               initWithItems:@[ action, fontSize ]];
-        [self addSubview:actionsFontSize];
-        _actionsFontSizeControl = actionsFontSize;
-        
-        [self configureAppearance];
-    }
-    return self;
-}
-
-- (void)configureAppearance
-{
-    UIImage *back = [[UIImage imageNamed:@"pagebar.png"]
-                     resizableImageWithCapInsets:UIEdgeInsetsZero];
-    self.backgroundColor = [UIColor colorWithPatternImage:back];
+    if (!(self = [super initWithFrame:frame])) return nil;
+    self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
     
-    UIImage *button = [[UIImage imageNamed:@"pagebar-button.png"]
-                       resizableImageWithCapInsets:UIEdgeInsetsMake(0, 3, 0, 3)];
-    UIImage *selected = [[UIImage imageNamed:@"pagebar-button-selected.png"]
-                         resizableImageWithCapInsets:UIEdgeInsetsMake(0, 3, 0, 3)];
-    for (UISegmentedControl *seg in @[ self.backForwardControl, self.actionsFontSizeControl ]) {
-        [seg setBackgroundImage:button
-                       forState:UIControlStateNormal
-                     barMetrics:UIBarMetricsDefault];
-        [seg setBackgroundImage:selected
-                       forState:UIControlStateHighlighted
-                     barMetrics:UIBarMetricsDefault];
-        [seg setDividerImage:[UIImage imageNamed:@"pagebar-segmented-divider.png"]
-         forLeftSegmentState:UIControlStateNormal
-           rightSegmentState:UIControlStateNormal
-                  barMetrics:UIBarMetricsDefault];
-    }
-    [self.jumpToPageButton setBackgroundImage:button forState:UIControlStateNormal];
-    [self.jumpToPageButton setBackgroundImage:selected forState:UIControlStateHighlighted];
+    UIImage *previousPage = [UIImage imageNamed:@"arrowleft.png"];
+    previousPage.accessibilityLabel = @"Previous page";
+    UIImage *nextPage = [UIImage imageNamed:@"arrowright.png"];
+    nextPage.accessibilityLabel = @"Next page";
+    UISegmentedControl *backForwardControl = [[UISegmentedControl alloc]
+                                              initWithItems:@[ previousPage, nextPage ]];
+    [self addSubview:backForwardControl];
+    _backForwardControl = backForwardControl;
+    
+    UIButton *jumpToPage = [UIButton buttonWithType:UIButtonTypeCustom];
+    jumpToPage.titleLabel.font = [UIFont boldSystemFontOfSize:11];
+    jumpToPage.accessibilityHint = @"Jumps to a page";
+    [self addSubview:jumpToPage];
+    _jumpToPageButton = jumpToPage;
+    
+    UIImage *action = [UIImage imageNamed:@"action.png"];
+    action.accessibilityLabel = @"Thread actions";
+    UIImage *fontSize = [UIImage imageNamed:@"font-size.png"];
+    fontSize.accessibilityLabel = @"Adjust style";
+    UISegmentedControl *actionsFontSize = [[UISegmentedControl alloc]
+                                           initWithItems:@[ action, fontSize ]];
+    [self addSubview:actionsFontSize];
+    _actionsFontSizeControl = actionsFontSize;
+    
+    return self;
 }
 
 - (void)layoutSubviews
@@ -89,11 +61,38 @@
                                                CGRectGetMidY(self.bounds) + topOffset);
     self.jumpToPageButton.frame = CGRectIntegral(self.jumpToPageButton.frame);
     
-    self.actionsFontSizeControl.frame = CGRectMake(CGRectGetMaxX(self.bounds) - horizontalMargin - segWidth, 0,
-                                                  segWidth, itemHeight);
+    self.actionsFontSizeControl.frame = CGRectMake(CGRectGetMaxX(self.bounds) - horizontalMargin -
+                                                   segWidth, 0, segWidth, itemHeight);
     self.actionsFontSizeControl.center = CGPointMake(self.actionsFontSizeControl.center.x,
                                                      CGRectGetMidY(self.bounds) + topOffset);
     self.actionsFontSizeControl.frame = CGRectIntegral(self.actionsFontSizeControl.frame);
+}
+
+#pragma mark - NSObject
+
++ (void)initialize
+{
+    if (self != [AwfulPageBottomBar class]) return;
+    UISegmentedControl *seg = [UISegmentedControl appearanceWhenContainedIn:
+                               [AwfulPageBottomBar class], nil];
+    UIImage *buttonBack = [[UIImage imageNamed:@"pagebar-button.png"]
+                           resizableImageWithCapInsets:UIEdgeInsetsMake(0, 3, 0, 3)];
+    [seg setBackgroundImage:buttonBack
+                   forState:UIControlStateNormal
+                 barMetrics:UIBarMetricsDefault];
+    UIImage *buttonSelected = [[UIImage imageNamed:@"pagebar-button-selected.png"]
+                               resizableImageWithCapInsets:UIEdgeInsetsMake(0, 3, 0, 3)];
+    [seg setBackgroundImage:buttonSelected
+                   forState:UIControlStateHighlighted
+                 barMetrics:UIBarMetricsDefault];
+    [seg setDividerImage:[UIImage imageNamed:@"pagebar-segmented-divider.png"]
+     forLeftSegmentState:UIControlStateNormal
+       rightSegmentState:UIControlStateNormal
+              barMetrics:UIBarMetricsDefault];
+    
+    UIButton *button = [UIButton appearanceWhenContainedIn:[AwfulPageBottomBar class], nil];
+    [button setBackgroundImage:buttonBack forState:UIControlStateNormal];
+    [button setBackgroundImage:buttonSelected forState:UIControlStateHighlighted];
 }
 
 @end
