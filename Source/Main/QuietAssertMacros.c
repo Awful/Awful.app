b16530ff3a0fece068148a7d441c9a33a05dcf62
Nolan Waite <nolan@nolanw.ca>
2013-09-05 04:41:05 -0300

    Fix copyright header.

--- Source/Main/QuietAssertMacros.c
+++ Source/Main/QuietAssertMacros.c
@@ -1,10 +1,6 @@
-//
 //  QuietAssertMacros.c
-//  Awful
 //
-//  Created by Nolan Waite on 2013-09-01.
-//  Copyright (c) 2013 Awful Contributors. All rights reserved.
-//
+//  Copyright 2013 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
 // Xcode 5 DP5 and DP6 spam the console like crazy. This is a workaround.
 // https://gist.github.com/OdNairy/6219906
