2dcabc9af4f53ca6f96a880f4d9d942e2b5e6c7b
Nolan Waite <nolan@nolanw.ca>
2013-09-06 07:30:14 -0300

    Fix copyright atop source files.

--- Source/View Controllers/AwfulVerticalTabBar.m
+++ Source/View Controllers/AwfulVerticalTabBar.m
@@ -1,10 +1,6 @@
-//
 //  AwfulVerticalTabBar.m
-//  Awful
 //
-//  Created by Nolan Waite on 2013-09-05.
-//  Copyright (c) 2013 Awful Contributors. All rights reserved.
-//
+//  Copyright 2013 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
 #import "AwfulVerticalTabBar.h"
 #import "AwfulTabBarButton.h"
