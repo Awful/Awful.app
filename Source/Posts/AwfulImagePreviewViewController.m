78387e78c9eded26601569738dd8b6ce79fc15f7
Simon Frost <simon.frost@accenture.com>
2013-09-20 00:43:33 +0100

    Allow images to be copied to the pasteboard

--- Source/Posts/AwfulImagePreviewViewController.m
+++ Source/Posts/AwfulImagePreviewViewController.m
@@ -171,6 +171,10 @@
         }];
         [self hideBarsAfterShortDuration];
     }];
+    [sheet addButtonWithTitle:@"Copy Image to Clipboard" block:^{
+        [UIPasteboard generalPasteboard].image = self.imageView.image;
+        [self hideBarsAfterShortDuration];
+    }];
     [sheet addCancelButtonWithTitle:@"Cancel" block:^{
         [self hideBarsAfterShortDuration];
     }];
