0c73823defe52dfb450e3bfdfe1d9df86dd9b655
Nolan Waite <nolan@nolanw.ca>
2013-10-13 18:42:49 -0300

    Fix AwfulBookmarksController import.

--- Source/Threads/AwfulBookmarksController.h
+++ Source/Threads/AwfulBookmarksController.h
@@ -2,7 +2,7 @@
 //
 //  Copyright 2010 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
-#import "AwfulThreadListController.h"
+#import "AwfulFetchedTableViewController.h"
 
 @interface AwfulBookmarksController : AwfulFetchedTableViewController
 
