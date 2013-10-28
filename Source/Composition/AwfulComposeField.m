2b354aed73064c8622814e0b6dca2b94e674e513
Nolan Waite <nolan@nolanw.ca>
2013-10-27 21:47:16 -0300

    Move AwfulComposeField to Composition.

---
+++ Source/Composition/AwfulComposeField.m
@@ -0,0 +1,42 @@
+//  AwfulComposeField.m
+//
+//  Copyright 2013 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
+
+#import "AwfulComposeField.h"
+
+@implementation AwfulComposeField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if (!(self = [super initWithFrame:frame])) return nil;
+    _label = [UILabel new];
+    _label.translatesAutoresizingMaskIntoConstraints = NO;
+    _label.font = [UIFont systemFontOfSize:16];
+    [_label setContentHuggingPriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
+    [_label setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+    [self addSubview:_label];
+    _textField = [UITextField new];
+    _textField.translatesAutoresizingMaskIntoConstraints = NO;
+    _textField.font = [UIFont systemFontOfSize:16];
+    [self addSubview:_textField];
+    NSDictionary *views = @{ @"label": _label,
+                             @"textField": _textField };
+    [self addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-4-[label]-[textField]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]];
+    [self addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[label]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]];
+    [self addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[textField]|"
+                                             options:0
+                                             metrics:nil
+                                               views:views]];
+    return self;
+}
+
+@end
