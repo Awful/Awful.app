ef1b461d7ea3e0e16e2128ce6bce73265e194af6
David Ridilla <davidridilla@gmail.com>
2014-03-05 16:11:20 -0500

    New Action Item Tint Colors

--- Source/Action Sheets/AwfulIconActionItem.m
+++ Source/Action Sheets/AwfulIconActionItem.m
@@ -27,85 +27,85 @@
     switch (type) {
         case AwfulIconActionItemTypeAddBookmark:
             title = @"Add Bookmark";
-            tintColor = [UIColor colorWithHue:0.206 saturation:0.816 brightness:0.639 alpha:1];
+            tintColor = [UIColor colorWithRed:0.463 green:0.702 blue:0.357 alpha:1.0];
             icon = [UIImage imageNamed:@"add-bookmark"];
             break;
         
         case AwfulIconActionItemTypeCopyURL:
             title = @"Copy\nURL";
-            tintColor = [UIColor colorWithHue:0.590 saturation:0.630 brightness:0.890 alpha:1];
+            tintColor = [UIColor colorWithRed:0.471 green:0.588 blue:0.725 alpha:1.0];
             icon = [UIImage imageNamed:@"copy-url"];
             break;
             
         case AwfulIconActionItemTypeEditPost:
             title = @"Edit\nPost";
-            tintColor = [UIColor colorWithHue:0.068 saturation:0.808 brightness:0.573 alpha:1];
+            tintColor = [UIColor colorWithRed:0.788 green:0.549 blue:0.388 alpha:1.0];
             icon = [UIImage imageNamed:@"edit-post"];
             break;
         
         case AwfulIconActionItemTypeJumpToFirstPage:
             title = @"Jump to First Page";
-            tintColor = [UIColor colorWithHue:0.153 saturation:0.111 brightness:0.882 alpha:1];
+            tintColor = [UIColor colorWithRed:0.565 green:0.545 blue:0.506 alpha:1.0];
             icon = [UIImage imageNamed:@"jump-to-first-page"];
             break;
         
         case AwfulIconActionItemTypeJumpToLastPage:
             title = @"Jump to Last Page";
-            tintColor = [UIColor colorWithHue:0.115 saturation:0.113 brightness:0.451 alpha:1];
+            tintColor = [UIColor colorWithRed:0.565 green:0.545 blue:0.506 alpha:1.0];
             icon = [UIImage imageNamed:@"jump-to-last-page"];
             break;
         
         case AwfulIconActionItemTypeMarkAsUnread:
             title = @"Mark as Unread";
-            tintColor = [UIColor colorWithHue:0.762 saturation:0.821 brightness:0.831 alpha:1];
+            tintColor = [UIColor colorWithRed:0.561 green:0.431 blue:0.667 alpha:1.0];
             icon = [UIImage imageNamed:@"mark-as-unread"];
             break;
             
         case AwfulIconActionItemTypeMarkReadUpToHere:
             title = @"Mark Read Up to Here";
-            tintColor = [UIColor colorWithHue:0.340 saturation:0.750 brightness:0.690 alpha:1];
+            tintColor = [UIColor colorWithRed:0.463 green:0.702 blue:0.357 alpha:1.0];
             icon = [UIImage imageNamed:@"mark-read-up-to-here"];
             break;
 
         case AwfulIconActionItemTypeQuotePost:
             title = @"Quote\nPost";
-            tintColor = [UIColor colorWithWhite:0.7 alpha:1];
+            tintColor = [UIColor colorWithRed:0.698 green:0.698 blue:0.698 alpha:1.0];
             icon = [UIImage imageNamed:@"quote-post"];
             break;
 
         case AwfulIconActionItemTypeRapSheet:
             title = @"Rap\nSheet";
-            tintColor = [UIColor colorWithHue:0.117 saturation:1.0 brightness:0.96 alpha:1];
+            tintColor = [UIColor colorWithRed:0.863 green:0.549 blue:0.243 alpha:1.0];
             icon = [UIImage imageNamed:@"rap-sheet"];
             break;
         
         case AwfulIconActionItemTypeRemoveBookmark:
             title = @"Remove Bookmark";
-            tintColor = [UIColor colorWithHue:0.023 saturation:0.85 brightness:0.835 alpha:1];
+            tintColor = [UIColor colorWithRed:0.831 green:0.333 blue:0.239 alpha:1.0];
             icon = [UIImage imageNamed:@"remove-bookmark"];
             break;
             
         case AwfulIconActionItemTypeSendPrivateMessage:
             title = @"Send\nPM";
-            tintColor = [UIColor colorWithHue:0.133 saturation:0.78 brightness:0.89 alpha:1];
+            tintColor = [UIColor colorWithRed:0.51 green:0.631 blue:0.604 alpha:1.0];
             icon = [UIImage imageNamed:@"send-private-message"];
             break;
             
         case AwfulIconActionItemTypeSingleUsersPosts:
             title = @"Just Their Posts";
-            tintColor = [UIColor colorWithHue:0.523 saturation:0.831 brightness:0.675 alpha:1];
+            tintColor = [UIColor colorWithRed:0.416 green:0.561 blue:0.584 alpha:1.0];
             icon = [UIImage imageNamed:@"single-users-posts"];
             break;
         
         case AwfulIconActionItemTypeUserProfile:
             title = @"User\nProfile";
-            tintColor = [UIColor colorWithHue:0.633 saturation:0.055 brightness:0.718 alpha:1];
+            tintColor = [UIColor colorWithRed:0.62 green:0.627 blue:0.667 alpha:1.0];
             icon = [UIImage imageNamed:@"user-profile"];
             break;
         
         case AwfulIconActionItemTypeVote:
             title = @"Vote\nThread";
-            tintColor = [UIColor colorWithHue:0.081 saturation:0.843 brightness:0.898 alpha:1];
+            tintColor = [UIColor colorWithRed:0.835 green:0.749 blue:0.353 alpha:1.0];
             icon = [UIImage imageNamed:@"vote"];
             break;
     }
