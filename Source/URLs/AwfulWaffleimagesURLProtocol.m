a66dc6928aaca16d47e409d30d95e1abd0a11723
Nolan Waite <nolan@nolanw.ca>
2014-04-29 18:01:30 -0300

    Fix copyright.

--- Source/URLs/AwfulWaffleimagesURLProtocol.m
+++ Source/URLs/AwfulWaffleimagesURLProtocol.m
@@ -1,10 +1,6 @@
-//
 //  AwfulWaffleimagesURLProtocol.m
-//  Awful
 //
-//  Created by Nolan Waite on 2014-04-24.
-//  Copyright (c) 2014 Awful Contributors. All rights reserved.
-//
+//  Copyright 2014 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
 #import "AwfulWaffleimagesURLProtocol.h"
 
