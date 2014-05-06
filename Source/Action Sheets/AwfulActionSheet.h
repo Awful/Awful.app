4989cda0a48c3e36e5c062c90b96acbd1526bc8f
Nolan Waite <nolan@nolanw.ca>
2014-05-06 05:32:31 -0300

    Document AwfulActionSheet.

--- Source/Action Sheets/AwfulActionSheet.h
+++ Source/Action Sheets/AwfulActionSheet.h
@@ -4,22 +4,44 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ * An AwfulActionSheet adds block-based methods to UIActionSheet.
+ */
 @interface AwfulActionSheet : UIActionSheet
 
-// Designated initializer.
+/**
+ * Designated initializer.
+ */
 - (id)initWithTitle:(NSString *)title;
 
-// UIActionSheet's designated initializer is re-implemented to ignore all parameters except title
-// and delegate.
+/**
+ * Ignores all parameters except title and delegate.
+ */
+- (id)initWithTitle:(NSString *)title delegate:(id<UIActionSheetDelegate>)delegate cancelButtonTitle:(NSString *)cancelButtonTitle destructiveButtonTitle:(NSString *)destructiveButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ...;
 
+/**
+ * Add a button and call a block if it's chosen.
+ */
 - (void)addButtonWithTitle:(NSString *)title block:(void (^)(void))block;
 
+/**
+ * Add a destructive button and call a block if it's chosen.
+ */
 - (void)addDestructiveButtonWithTitle:(NSString *)title block:(void (^)(void))block;
 
+/**
+ * Add a cancel button and call a block if it's chosen.
+ */
 - (void)addCancelButtonWithTitle:(NSString *)title block:(void (^)(void))block;
 
+/**
+ * Add a cancel button.
+ */
 - (void)addCancelButtonWithTitle:(NSString *)title;
 
+/**
+ * Call a block after the action sheet is dismissed.
+ */
 - (void)setCompletionBlock:(void (^)(void))completionBlock;
 
 @end
