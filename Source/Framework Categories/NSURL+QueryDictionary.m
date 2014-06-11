72d201f1fcd0751609cf8f491bd8d751aea75fb5
Nolan Waite <nolan@nolanw.ca>
2014-06-11 02:03:46 -0300

    Don't crash in -[NSURL queryDictionary] when no equals signs are in the query string.

--- Source/Framework Categories/NSURL+QueryDictionary.m
+++ Source/Framework Categories/NSURL+QueryDictionary.m
@@ -9,10 +9,12 @@
 - (NSDictionary *)queryDictionary
 {
     NSMutableDictionary *queryDictionary = [NSMutableDictionary new];
-    for (NSString *pair in [[self query] componentsSeparatedByString:@"&"]) {
+    for (NSString *pair in [self.query componentsSeparatedByString:@"&"]) {
         if (pair.length == 0) continue;
         NSArray *keyAndValue = [pair componentsSeparatedByString:@"="];
-        queryDictionary[keyAndValue[0]] = keyAndValue[1];
+        NSString *value = @"";
+        if (keyAndValue.count > 1) value = keyAndValue[1];
+        queryDictionary[keyAndValue[0]] = value;
     }
     return queryDictionary;
 }
