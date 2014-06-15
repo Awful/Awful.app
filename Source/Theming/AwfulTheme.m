456a88d5df721bebb8487d516d202dba47f06bde
Nolan Waite <nolan@nolanw.ca>
2014-06-15 08:15:51 -0300

    Tidy up AwfulFrameworkCategories.h.
    * Forgot UIColor category.
    * Alphabetize.

--- Source/Theming/AwfulTheme.m
+++ Source/Theming/AwfulTheme.m
@@ -3,9 +3,9 @@
 //  Copyright 2013 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
 #import "AwfulTheme.h"
+#import "AwfulFrameworkCategories.h"
 #import "AwfulSettings.h"
 #import "AwfulThemeLoader.h"
-#import "UIColor+AwfulConvenience.h"
 
 @implementation AwfulTheme
 
