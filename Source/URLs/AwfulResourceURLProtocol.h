de9134eb7cfd290bad70cc24dd5f995fc75873e9
Nolan Waite <nolan@nolanw.ca>
2014-07-08 02:08:51 -0300

    Formatting.

--- Source/URLs/AwfulResourceURLProtocol.h
+++ Source/URLs/AwfulResourceURLProtocol.h
@@ -2,7 +2,7 @@
 //
 //  Copyright 2014 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 /**
  * AwfulResourceURLProtocol implements the `awful-resource:` URL scheme, which loads resources from the main bundle.
