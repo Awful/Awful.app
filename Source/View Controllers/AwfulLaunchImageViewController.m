79bb9e6b9ebbbd791b7738aa9d791d4b307bfcea
Nolan Waite <nolan@nolanw.ca>
2014-08-15 02:59:40 -0300

    Fix iOS 8 deprecations.

--- Source/View Controllers/AwfulLaunchImageViewController.m
+++ Source/View Controllers/AwfulLaunchImageViewController.m
@@ -41,11 +41,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-    self.imageView.highlighted = UIInterfaceOrientationIsLandscape(self.interfaceOrientation);
-    #pragma clang diagnostic pop
+    self.imageView.highlighted = CGRectGetWidth(self.view.bounds) == self.imageView.highlightedImage.size.width;
 }
 
 - (UIImageView *)imageView
@@ -53,27 +49,21 @@
     return (UIImageView *)self.view;
 }
 
-- (UIStatusBarStyle)preferredStatusBarStyle
-{
-    return UIStatusBarStyleLightContent;
-}
-
 - (NSUInteger)supportedInterfaceOrientations
 {
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+    if (self.traitCollection.userInterfaceIdiom == UIUserInterfaceIdiomPad) {
         return UIInterfaceOrientationMaskAll;
     } else {
         return (UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskPortraitUpsideDown);
     }
 }
 
-- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator
 {
-    [UIView transitionWithView:self.imageView
-                      duration:duration
-                       options:UIViewAnimationOptionTransitionCrossDissolve
-                    animations:^{ self.imageView.highlighted = UIInterfaceOrientationIsLandscape(toInterfaceOrientation); }
-                    completion:nil];
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    [coordinator animateAlongsideTransition:^(id <UIViewControllerTransitionCoordinatorContext> context) {
+        self.imageView.highlighted = size.width == self.imageView.highlightedImage.size.width;
+    } completion:nil];
 }
 
 @end
