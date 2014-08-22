f63fa71104299081c533a5806b1fe1273548ec23
Nolan Waite <nolan@nolanw.ca>
2014-08-22 14:05:16 -0300

    Redo posts page settings view for iOS 8 presentation.

--- Source/Main/Awful-Bridging-Header.h
+++ Source/Main/Awful-Bridging-Header.h
@@ -2,5 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "AwfulModels.h"
 #import "AwfulPostsViewController.h"
+#import "AwfulSettings.h"
+#import "AwfulThemeLoader.h"
+#import "AwfulThemePicker.h"
 #import "UIViewController+AwfulTheme.h"
