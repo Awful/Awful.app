b2535508bb7aff00cdd5a567d07b461949380463
Nolan Waite <nolan@nolanw.ca>
2014-08-29 15:02:21 -0300

    Lay out forum list using auto layout.

---
+++ Source/Forums/FavoriteForumCell.swift
@@ -0,0 +1,10 @@
+//  FavoriteForumCell.swift
+//
+//  Copyright 2014 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
+
+import UIKit
+
+class FavoriteForumCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+}
