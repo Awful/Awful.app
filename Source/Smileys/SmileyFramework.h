2aa920e78f52761e2404fd9708b800dd2676658b
Nolan Waite <nolan@nolanw.ca>
2014-09-24 17:02:38 -0300

    Add target for smiley custom keyboard extension.
    * Also add a framework for common smiley keyboard code between the app and the extension.
    * Coerce command-line test runner to work with smiley framework tests.
    * Bump project-wide deployment target to iOS 8.0.

---
+++ Source/Smileys/SmileyFramework.h
@@ -0,0 +1,13 @@
+//  SmileyFramework.h
+//
+//  Copyright 2014 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmileyFramework.
+FOUNDATION_EXPORT double SmileyFrameworkVersionNumber;
+
+//! Project version string for SmileyFramework.
+FOUNDATION_EXPORT const unsigned char SmileyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmileyFramework/PublicHeader.h>
