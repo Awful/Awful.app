66dac4e0124e49ce21bc6b727a9459ebfab9062c
Nolan Waite <nolan@nolanw.ca>
2014-09-26 20:07:23 -0300

    Extract SmileyDownloader into its own file.

---
+++ Source/Smileys/SmileyDownloader.swift
@@ -0,0 +1,40 @@
+//  SmileyDownloader.swift
+//
+//  Copyright 2014 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
+
+import Foundation
+
+public protocol SmileyDownloader {
+    func downloadImageDataFromURL(URL: NSURL, completionBlock: (imageData: NSData!, error: NSError!) -> Void)
+}
+
+class URLSessionSmileyDownloader: SmileyDownloader {
+    lazy private var session: NSURLSession = {
+        let configuration = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        configuration.networkServiceType = .NetworkServiceTypeBackground
+        return NSURLSession(configuration: configuration)
+        }()
+    
+    func downloadImageDataFromURL(URL: NSURL, completionBlock: (imageData: NSData!, error: NSError!) -> Void) {
+        let task = session.dataTaskWithURL(URL) { imageData, response, error in
+            if let response = response as? NSHTTPURLResponse {
+                if 200..<299 ~= (response as NSHTTPURLResponse).statusCode {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completionBlock(imageData: imageData, error: nil)
+                    }
+                } else {
+                    let error = NSError(domain: "SmileyErrorDomain", code: 1, userInfo: ["HTTPResponseKey": response])
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completionBlock(imageData: nil, error: error)
+                    }
+                }
+            } else {
+                let error = NSError(domain: "SmileyErrorDomain", code: 1, userInfo: [NSUnderlyingErrorKey: error])
+                dispatch_async(dispatch_get_main_queue()) {
+                    completionBlock(imageData: nil, error: error)
+                }
+            }
+        }
+        task.resume()
+    }
+}
--- Source/Smileys/SmileyUpdater.swift
+++ Source/Smileys/SmileyUpdater.swift
@@ -68,42 +68,6 @@ public class SmileyUpdater: NSObject {
     }
 }
 
-
-public protocol SmileyDownloader {
-    func downloadImageDataFromURL(URL: NSURL, completionBlock: (imageData: NSData!, error: NSError!) -> Void)
-}
-
-private class URLSessionSmileyDownloader: SmileyDownloader {
-    lazy var session: NSURLSession = {
-        let configuration = NSURLSessionConfiguration.ephemeralSessionConfiguration()
-        configuration.networkServiceType = .NetworkServiceTypeBackground
-        return NSURLSession(configuration: configuration)
-    }()
-    
-    func downloadImageDataFromURL(URL: NSURL, completionBlock: (imageData: NSData!, error: NSError!) -> Void) {
-        let task = session.dataTaskWithURL(URL) { imageData, response, error in
-            if let response = response as? NSHTTPURLResponse {
-                if 200..<299 ~= (response as NSHTTPURLResponse).statusCode {
-                    dispatch_async(dispatch_get_main_queue()) {
-                        completionBlock(imageData: imageData, error: nil)
-                    }
-                } else {
-                    let error = NSError(domain: "SmileyErrorDomain", code: 1, userInfo: ["HTTPResponseKey": response])
-                    dispatch_async(dispatch_get_main_queue()) {
-                        completionBlock(imageData: nil, error: error)
-                    }
-                }
-            } else {
-                let error = NSError(domain: "SmileyErrorDomain", code: 1, userInfo: [NSUnderlyingErrorKey: error])
-                dispatch_async(dispatch_get_main_queue()) {
-                    completionBlock(imageData: nil, error: error)
-                }
-            }
-        }
-        task.resume()
-    }
-}
-
 private class NewSmileyObserver {
     let managedObjectContext: NSManagedObjectContext
     let notificationBlock: ([SmileyPrimaryKey:NSURL]) -> Void
