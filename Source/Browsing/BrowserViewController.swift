10110d4af71c5edbbfbefb7b90e6cad8c8a04e66
Nolan Waite <nolan@nolanw.ca>
2014-12-05 19:14:28 -0400

    Present browser modally on all devices.
    Was previously pushing on to the navigation stack on iPhone. The new browser control messes with the navigation bar and such and doesn't put it back, unfortunately, so this is my quick workaround.

--- Source/Browsing/BrowserViewController.swift
+++ Source/Browsing/BrowserViewController.swift
@@ -6,15 +6,10 @@ final class BrowserViewController: DZNWebViewController {
     class func presentBrowserForURL(URL: NSURL, fromViewController presentingViewController: UIViewController) -> BrowserViewController {
         let browser = BrowserViewController(URL: URL)
         browser.restorationIdentifier = "Awful Browser"
-        if UIDevice.currentDevice().userInterfaceIdiom == .Pad || presentingViewController.navigationController == nil {
-            let navigation = browser.enclosingNavigationController
-            let closeButton = UIBarButtonItem(title: "Close", style: .Plain, target: browser, action: "close")
-            browser.navigationItem.leftBarButtonItem = closeButton
-            presentingViewController.presentViewController(navigation, animated: true, completion: nil)
-        } else {
-            let navigation = presentingViewController.navigationController!
-            navigation.pushViewController(browser, animated: true)
-        }
+        let navigation = browser.enclosingNavigationController
+        let closeButton = UIBarButtonItem(title: "Close", style: .Plain, target: browser, action: "close")
+        browser.navigationItem.leftBarButtonItem = closeButton
+        presentingViewController.presentViewController(navigation, animated: true, completion: nil)
         return browser
     }
     
