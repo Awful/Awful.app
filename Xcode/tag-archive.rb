6e2a66168bfdd68c13ed3986914796fca9ed297c
Nolan Waite <nolan@nolanw.ca>
2015-02-10 01:29:18 -0400

    Fix archive-commenting script.
    Not sure why I thought the archive products path was coming via argv. As for the bottommost change, I don't get ruby.

--- Xcode/tag-archive.rb
+++ Xcode/tag-archive.rb
@@ -2,25 +2,24 @@
 require 'fileutils'
 require 'pathname'
 
-ARCHIVE = Pathname.new(ARGV[0])
+ARCHIVE = Pathname.new(ENV['ARCHIVE_PRODUCTS_PATH']).dirname
 INFO_PLIST = ARCHIVE + "Info.plist"
 
 def buddy(command)
-  IO.popen(["/usr/libexec/PlistBuddy", "-c", command, INFO_PLIST.to_s]) { |io|
+  IO.popen(["/usr/libexec/PlistBuddy", "-c", command, INFO_PLIST.to_s]) do |io|
     io.read.strip
-  }
+  end
 end
 
-short_version = buddy("Print :ApplicationProperties:CFBundleShortVersionString")
+short_version = buddy("Print :ApplicationProperties:CFBundleShortVersionString").strip
 bundle_version = buddy("Print :ApplicationProperties:CFBundleVersion").to_i
 
 comment = "#{short_version}-beta#{bundle_version % 100}"
 buddy "Add :Comment string \"#{comment}\""
 
-ARCHIVE_DIR = ARCHIVE.dirname
-TMP_ARCHIVE = ARCHIVE_DIR + "#{ARCHIVE.basename}tmp"
+TMP_ARCHIVE = ARCHIVE.dirname + "#{ARCHIVE.basename}tmp"
 
 # This stupidity works around the Xcode Organizer not picking up on changes. It does, however, pick up on folders disappearing and reappearing!
-FileUtils.mv ARCHIVE TMP_ARCHIVE
+FileUtils.mv(ARCHIVE, TMP_ARCHIVE)
 sleep 1
-FileUtils.mv TMP_ARCHIVE ARCHIVE
+FileUtils.mv(TMP_ARCHIVE, ARCHIVE)
