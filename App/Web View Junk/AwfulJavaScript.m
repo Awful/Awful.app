3cfdfd7e06e38c6e45f33e85f02ca14116c37baa
Nolan Waite <nolan@nolanw.ca>
2015-03-19 20:02:30 -0300

    Fix rendering posts.
    WebViewJavascriptBridge.js.txt moved to its framework and wasn't being loaded.

--- App/Web View Junk/AwfulJavaScript.m
+++ App/Web View Junk/AwfulJavaScript.m
@@ -3,12 +3,18 @@
 //  Copyright 2014 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
 #import "AwfulJavaScript.h"
+#import <WebViewJavascriptBridge/WebViewJavascriptBridge.h>
 
 NSString * LoadJavaScriptResources(NSArray *filenames, NSError * __autoreleasing *error)
 {
     NSMutableArray *scripts = [NSMutableArray new];
     for (NSString *filename in filenames) {
         NSURL *URL = [[NSBundle mainBundle] URLForResource:filename withExtension:nil];
+        // Loading CocoaPods framework resources can be tricky. I'm not sure why this file doesn't end up in the Resources folder of the framework, but let's assume it will someday.
+        if ([filename isEqualToString:@"WebViewJavascriptBridge.js.txt"]) {
+            NSBundle *bundle = [NSBundle bundleForClass:[WebViewJavascriptBridge class]];
+            URL = [bundle URLForResource:filename withExtension:nil] ?: [bundle.bundleURL URLByAppendingPathComponent:filename];
+        }
         NSString *script = [NSString stringWithContentsOfURL:URL encoding:NSUTF8StringEncoding error:error];
         if (!script) {
             if (error && ![*error userInfo][NSURLErrorKey]) {
