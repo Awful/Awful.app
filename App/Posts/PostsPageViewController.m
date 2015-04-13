c27a56cfb95fd465722be5d1e8426d75bfb4e4a8
Mike Shaver <shaver@fb.com>
2015-04-13 08:58:15 -0700

    Give feedback after mark-read-to-here.

--- App/Posts/PostsPageViewController.m
+++ App/Posts/PostsPageViewController.m
@@ -814,6 +814,13 @@ typedef void (^ReplyCompletion)(BOOL, BOOL);
                 } else {
                     post.thread.seenPosts = post.threadIndex;
                     [self->_webViewJavaScriptBridge callHandler:@"markReadUpToPostWithID" data:post.postID];
+                    MRProgressOverlayView *overlay = [MRProgressOverlayView showOverlayAddedTo:self.view
+                                                                                         title:@"Marked Read"
+                                                                                          mode:MRProgressOverlayViewModeCheckmark
+                                                                                      animated:YES];
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.7 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                        [overlay dismiss:YES];
+                    });
                 }
             }];
         }]];
