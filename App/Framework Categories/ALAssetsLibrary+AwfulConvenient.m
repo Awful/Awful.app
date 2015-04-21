7fb915e2408898a42aa2d6b1d3bfb9ff8220fc3f
Nolan Waite <nolan@nolanw.ca>
2015-04-21 19:39:08 -0300

    Try harder not to get nil ALAsset from photo stream.
    This changed somewhere around iOS 8.2.

--- App/Framework Categories/ALAssetsLibrary+AwfulConvenient.m
+++ App/Framework Categories/ALAssetsLibrary+AwfulConvenient.m
@@ -12,8 +12,29 @@
     dispatch_semaphore_t flag = dispatch_semaphore_create(0);
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         [self assetForURL:URL resultBlock:^(ALAsset *blockAsset) {
-            asset = blockAsset;
-            dispatch_semaphore_signal(flag);
+            if (blockAsset) {
+                asset = blockAsset;
+                dispatch_semaphore_signal(flag);
+            } else {
+                // iOS 8 workaround for photo stream from http://stackoverflow.com/a/26526199/1063051
+                [self enumerateGroupsWithTypes:ALAssetsGroupPhotoStream usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+                    [group enumerateAssetsWithOptions:NSEnumerationReverse usingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
+                        if ([result.defaultRepresentation.url isEqual:URL]) {
+                            asset = result;
+                            *stop = YES;
+                        }
+                    }];
+                    if (asset) {
+                        *stop = YES;
+                    }
+                    if (!group) {
+                        dispatch_semaphore_signal(flag);
+                    }
+                } failureBlock:^(NSError *blockError) {
+                    *error = blockError;
+                    dispatch_semaphore_signal(flag);
+                }];
+            }
         } failureBlock:^(NSError *blockError) {
             if (error) *error = blockError;
             dispatch_semaphore_signal(flag);
