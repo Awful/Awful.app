761f2c2afb85b6bcd55afd23e18b1c567697e9c0
Nolan Waite <nolan@nolanw.ca>
2015-10-18 23:17:13 -0300

    Add missing @import Foundation.

--- Core/Model/AwfulThreadPage.h
+++ Core/Model/AwfulThreadPage.h
@@ -2,6 +2,8 @@
 //
 //  Copyright 2013 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
+@import Foundation;
+
 typedef NS_ENUM(NSInteger, AwfulThreadPage) {
     // Not sure what the last page is, but you want it.
     AwfulThreadPageLast = -2,
