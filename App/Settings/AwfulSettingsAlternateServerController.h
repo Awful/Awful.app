bf483e68ff9b59bc4323263105a613b73d208081
Nolan Waite <nolan@nolanw.ca>
2016-01-19 21:43:56 -0400

    Delete remnants of "alternate server" setting.
    It's unreachable from the UI and the default server is hardcoded now anyway.

--- App/Settings/AwfulSettingsAlternateServerController.h
+++
@@ -1,9 +0,0 @@
-//  AwfulSettingsAlternateServerController.h
-//
-//  Copyright 2013 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
-
-#import "AwfulViewController.h"
-
-@interface AwfulSettingsAlternateServerController : AwfulTableViewController
-
-@end
