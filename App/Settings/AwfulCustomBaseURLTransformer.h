096cdab2f0dfe9ffcb98c1a51873869553aeb2cf
Nolan Waite <nolan@nolanw.ca>
2016-04-09 14:12:05 -0300

    Delete unused class AwfulCustomBaseURLTransformer.

--- App/Settings/AwfulCustomBaseURLTransformer.h
+++
@@ -1,9 +0,0 @@
-//  AwfulCustomBaseURLTransformer.h
-//
-//  Copyright 2013 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
-
-@import Foundation;
-
-@interface AwfulCustomBaseURLTransformer : NSValueTransformer
-
-@end
