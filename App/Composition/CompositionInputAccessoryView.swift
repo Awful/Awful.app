640ad4b9528d76903cd61435cc9938d18ebd62bd
Nolan Waite <nolan@nolanw.ca>
2016-05-31 19:17:11 -0300

    Fix incorrect keyboard bar height on iPad.

--- App/Composition/CompositionInputAccessoryView.swift
+++ App/Composition/CompositionInputAccessoryView.swift
@@ -49,7 +49,7 @@ final class CompositionInputAccessoryView: UIInputView {
         smilieCommand = ShowSmilieKeyboardCommand(textView: textView)
         autocloseCommand = CloseBBcodeTagCommand(textView: textView)
         
-        let height = textView.traitCollection.userInterfaceIdiom == .Pad ? 66 : 38
+        let height = UIDevice.currentDevice().userInterfaceIdiom == .Pad ? 66 : 38
         let frame = CGRect(x: 0, y: 0, width: 0, height: height)
         super.init(frame: frame, inputViewStyle: .Default)
         
