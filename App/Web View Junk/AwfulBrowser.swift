01facf6fe3445d67dbd7ed114fc67a087f0c7220
Nolan Waite <nolan@nolanw.ca>
2016-11-07 22:16:09 -0400

    Fix "Open in Chrome" activity in Awful Browser.
    Would open Chrome app but the URL wouldn't load. Turns out Safari View Controller doesn't call an expected method on the UIActivity.

--- App/Web View Junk/AwfulBrowser.swift
+++ App/Web View Junk/AwfulBrowser.swift
@@ -18,7 +18,38 @@ final class AwfulBrowser: NSObject {
 }
 
 extension AwfulBrowser: SFSafariViewControllerDelegate {
-    func safariViewController(_ controller: SFSafariViewController, activityItemsFor URL: URL, title: String?) -> [UIActivity] {
-        return [ARChromeActivity()]
+    func safariViewController(_ controller: SFSafariViewController, activityItemsFor url: URL, title: String?) -> [UIActivity] {
+        return [Rdar24138390ChromeActivity(url: url)]
+    }
+}
+
+/// Activity items in SFSafariViewController aren't sent `prepare(withActivityItems:)` for some unknown reason, so we work around that here.
+private final class Rdar24138390ChromeActivity: ARChromeActivity {
+    private let url: URL
+    
+    init(url: URL) {
+        self.url = url
+        super.init()
+    }
+    
+    override func canPerform(withActivityItems activityItems: [Any]) -> Bool {
+        for item in activityItems {
+            guard let activityURL = item as? URL else { continue }
+            return activityURL.absoluteURL == url.absoluteURL
+        }
+        
+        return false
+    }
+    
+    override func perform() {
+        // `ARChromeActivity`, on advice from the documentation, uses `prepare(withActivityItems:)` to save the activity item being acted upon.
+        prepare(withActivityItems: [url])
+        
+        super.perform()
+    }
+    
+    override var activityImage: UIImage? {
+        // Superclass checks in bundle for `self.class` instead of hardcoding `ARChromeActivity.self` so we need to unbreak that.
+        return UIImage(named: "ARChromeActivity", in: Bundle(for: ARChromeActivity.self), compatibleWith: nil)
     }
 }
