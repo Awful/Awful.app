0f031d2c847ecd94dd71b0cae4e8430bee7bcf7c
Nolan Waite <nolan@nolanw.ca>
2017-09-15 17:20:31 -0300

    Update pod PromiseKit.

--- Pods/PromiseKit/Sources/AnyPromise.swift
+++ Pods/PromiseKit/Sources/AnyPromise.swift
@@ -37,7 +37,7 @@ import Foundation
      - Note: A “void” `AnyPromise` has a value of `nil`.
     */
     convenience public init(_ bridge: Promise<Void>) {
-        self.init(force: bridge.then(on: zalgo) { _ in nil })
+        self.init(force: bridge.then(on: zalgo) { nil })
     }
 
     /**
@@ -206,9 +206,9 @@ import Foundation
         })
     }
 
-    @objc func __catchWithPolicy(_ policy: CatchPolicy, execute body: @escaping (Any?) -> Any?) -> AnyPromise {
+    @objc func __catchOn(_ q: DispatchQueue, withPolicy policy: CatchPolicy, execute body: @escaping (Any?) -> Any?) -> AnyPromise {
         return AnyPromise(sealant: { resolve in
-            state.catch(on: .default, policy: policy, else: resolve) { err in
+            state.catch(on: q, policy: policy, else: resolve) { err in
                 makeHandler(body, resolve)(err as NSError)
             }
         })
@@ -246,7 +246,7 @@ import Foundation
             switch resolution {
             case .rejected(let error, let token):
                 token.consumed = true  // when and join will create a new parent error that is unconsumed
-                body(error as Error)
+                body(error as NSError)
             case .fulfilled(let value):
                 body(value)
             }
