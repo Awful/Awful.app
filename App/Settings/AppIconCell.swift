46dd6b260b968b374986e9ad2818e3dceea20ef5
Nolan Waite <nolan@nolanw.ca>
2017-10-01 16:36:03 -0300

    Mask the alternate app icons in the picker and show selected using the blue tick.

--- App/Settings/AppIconCell.swift
+++ App/Settings/AppIconCell.swift
@@ -10,15 +10,23 @@ import UIKit
 
 final class AppIconCell: UICollectionViewCell {
     @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var isSelectedIcon: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        imageView.layer.mask = makeSquircleMask()
+    }
 
     func configure(image: UIImage?, isCurrentlySelectedIcon: Bool) {
         imageView.image = image
-
-        if isCurrentlySelectedIcon {
-            imageView.layer.borderWidth = 6
-            imageView.layer.borderColor = (Theme.currentTheme["tintColor"]! as UIColor).cgColor
-        } else {
-            imageView.layer.borderWidth = 0
-        }
+        isSelectedIcon.isHidden = !isCurrentlySelectedIcon
     }
+}
+
+private func makeSquircleMask() -> CALayer {
+    let shape = CAShapeLayer()
+    shape.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: 60, height: 60), cornerRadius: 13).cgPath
+    shape.fillColor = UIColor.black.cgColor
+    return shape
 }
