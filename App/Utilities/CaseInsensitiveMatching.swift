14c1b40fcefddbec0749f144a36458d4113083d8
Nolan Waite <nolan@nolanw.ca>
2018-03-11 21:58:02 -0300

    Make CaseInsensitive matcher a little more useful.

--- App/Utilities/CaseInsensitiveMatching.swift
+++ App/Utilities/CaseInsensitiveMatching.swift
@@ -2,13 +2,54 @@
 //
 //  Copyright 2016 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
+/**
+ Wraps a `String` for case-insensitive matching in a `switch` statement.
+
+ See `String.caseInsensitive` for example usage.
+ */
 struct CaseInsensitive {
-    let string: String
+    private let string: String
+
     init(_ string: String) {
         self.string = string
     }
+
+    static func ~= (pattern: String?, predicate: CaseInsensitive) -> Bool {
+        return pattern?.caseInsensitiveCompare(predicate.string) == .orderedSame
+    }
+
+    static func == (lhs: CaseInsensitive, rhs: String) -> Bool {
+        return lhs.string.caseInsensitiveCompare(rhs) == .orderedSame
+    }
 }
 
-func ~=(pattern: String?, predicate: CaseInsensitive) -> Bool {
-    return pattern?.caseInsensitiveCompare(predicate.string) == .orderedSame
+extension String {
+
+    /**
+     Returns a case-insensitive wrapper around the string suitable for a `switch` statement.
+
+     For example:
+
+         let scheme = "awFUL"
+         switch scheme.caseInsensitive {
+         case "awful":
+             print("Awful scheme!")
+         case "http", "https":
+             print("Hypertext Transfer Protocol!")
+         default:
+             break
+         }
+         // Prints "Awful scheme!"
+
+
+     There's also an equality operator if that makes something easier:
+
+         if scheme.caseInsensitive == "awful" {
+             print("hooray!")
+         }
+         // Prints "hooray!"
+     */
+    var caseInsensitive: CaseInsensitive {
+        return CaseInsensitive(self)
+    }
 }
