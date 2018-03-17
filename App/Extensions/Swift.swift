2e528b0191b709f06494d784b98cb11810c04b89
Nolan Waite <nolan@nolanw.ca>
2018-03-17 13:37:50 -0300

    Clean up Swift stdlib extensions.
    * Nothing uses Sequence.all.
    * Array.first is in the stdlib.
    * any and first module-level functions are unnecessary with instance methods on Sequence.
    * Sequence.any could use some commentary.

--- App/Extensions/Swift.swift
+++ App/Extensions/Swift.swift
@@ -3,16 +3,6 @@
 //  Copyright 2014 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
 extension Array {
-    /// Returns the first element for which the predicate returns true.
-    func first(_ predicate: (Element) -> Bool) -> Element? {
-        for item in self {
-            if predicate(item) {
-                return item
-            }
-        }
-        return nil
-    }
-    
     /// Same as reduce() but with the first element used as the initial accumulated value.
     func reduce(_ combine: (Element, Element) -> Element) -> Element? {
         if let initial = first {
@@ -36,30 +26,16 @@ extension Int {
 }
 
 extension Sequence {
-    func all(_ includeElement: (Iterator.Element) -> Bool) -> Bool {
-        for element in self where !includeElement(element) {
-            return false
-        }
-        return true
-    }
-    
+
+    /**
+     Returns `true` if `predicate` returns `true` for any element in the sequence.
+
+     This method stops as soon as `predicate` returns `true`.
+     */
     func any(where predicate: (_ element: Element) -> Bool) -> Bool {
         for element in self where predicate(element) {
             return true
         }
         return false
     }
-}
-
-func any<S: Sequence, T>(_ sequence: S, includeElement: (T) -> Bool) -> Bool where T == S.Iterator.Element {
-    return first(sequence, includeElement: includeElement) != nil
-}
-
-func first<S: Sequence, T>(_ sequence: S, includeElement: (T) -> Bool) -> T? where T == S.Iterator.Element {
-    for element in sequence {
-        if includeElement(element) {
-            return element
-        }
-    }
-    return nil
 }
