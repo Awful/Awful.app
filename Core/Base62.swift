f38f4f7fe5d710e229a9a882d28b259c9281a28b
Nolan Waite <nolan@nolanw.ca>
2018-11-22 23:13:56 -0400

    Remove Darwin dependency for base62Encode.

--- Core/Base62.swift
+++ Core/Base62.swift
@@ -6,22 +6,21 @@
 //  Copyright © 2017 Awful Contributors. All rights reserved.
 //
 
-import Darwin
+/// Returns a base-62 representation of an integer. Digits are 0-9, A-Z, a-z.
+internal func base62Encode<I: BinaryInteger>(_ i: I) -> String {
+    let base = I(base62Alphabet.count)
+    var digits: [Character] = []
+    var i = i
+    while i > base {
+        let digit: I
+        (i, digit) = i.quotientAndRemainder(dividingBy: base)
+        digits.append(base62Alphabet[Int(digit)])
+    }
+    digits.append(base62Alphabet[Int(i)])
+    return String(digits.reversed())
+}
 
 private let base62Alphabet: [Character] = [
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-internal func base62Encode(_ i: Int32) -> String {
-    let base = Int32(base62Alphabet.count)
-    var digits: [Character] = []
-    var i = i
-    while i > base {
-        let result = div(i, base)
-        i = result.quot
-        digits.append(base62Alphabet[Int(result.rem)])
-    }
-    digits.append(base62Alphabet[Int(i)])
-    return String(digits.reversed())
-}
