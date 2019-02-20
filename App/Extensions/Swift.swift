376baf7b840f4f50124a05723a08497678b2b338
Nolan Waite <nolan@nolanw.ca>
2019-02-20 00:10:31 -0400

    Make reduce(_:) variant that uses the first value a bit more general.

--- App/Extensions/Swift.swift
+++ App/Extensions/Swift.swift
@@ -2,17 +2,6 @@
 //
 //  Copyright 2014 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
-extension Array {
-    /// Same as reduce() but with the first element used as the initial accumulated value.
-    func reduce(_ combine: (Element, Element) -> Element) -> Element? {
-        if let initial = first {
-            return self.dropFirst().reduce(initial, combine)
-        } else {
-            return nil
-        }
-    }
-}
-
 extension Numeric where Self: Comparable {
     
     /// Returns `self` if it's within `range`, otherwise returns the bound of `range` nearest to `self`.
@@ -24,5 +13,21 @@ extension Numeric where Self: Comparable {
         } else {
             return self
         }
+    }
+}
+
+extension Sequence {
+    
+    /// Same as `reduce(_:_:)` but with the first element used as the initial result value. Returns `nil` when the sequence is empty.
+    func reduce(_ combine: (Element, Element) throws -> Element) rethrows -> Element? {
+        var iterator = makeIterator()
+        
+        guard var result = iterator.next() else { return nil }
+        
+        while let next = iterator.next() {
+            result = try combine(result, next)
+        }
+        
+        return result
     }
 }
