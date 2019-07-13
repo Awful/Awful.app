91bf02123dcfbaa045816105c32e9a513f8f6a58
Nolan Waite <nolan@nolanw.ca>
2019-07-13 15:53:49 -0300

    Delete unused code.

--- App/Composition/CompositionMenuTree.swift
+++ App/Composition/CompositionMenuTree.swift
@@ -382,13 +382,3 @@ fileprivate func isPickerAvailable(_ sourceType: UIImagePickerController.SourceT
         return UIImagePickerController.isSourceTypeAvailable(sourceType)
     }
 }
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
-	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-	return input.rawValue
-}
