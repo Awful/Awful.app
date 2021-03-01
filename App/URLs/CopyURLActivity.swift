98b7cffde26bd551ad4d87a4186df4187923ad49
Nolan Waite <nolan@nolanw.ca>
2021-02-28 23:29:54 -0400

    Tidy up "copy URL" activity.

--- App/URLs/CopyURLActivity.swift
+++ App/URLs/CopyURLActivity.swift
@@ -7,13 +7,6 @@ import UIKit
 /// Adds a "Copy URL" activity. Place the target URL in a `Box` before adding it to `activityItems` and no other activities will see or attempt to use the URL.
 final class CopyURLActivity: UIActivity {
 
-    private let overriddenTitle: String?
-
-    /// - Parameter title: Overrides the default "Copy URL" title.
-    init(title: String? = nil) {
-        overriddenTitle = title
-    }
-
     /// Wraps a URL so that only the `CopyURLActivity` will try to use it.
     final class Box {
         fileprivate let url: URL
@@ -26,29 +19,21 @@ final class CopyURLActivity: UIActivity {
     // MARK: UIActivity
     
     override var activityType: UIActivity.ActivityType {
-        return UIActivity.ActivityType("com.awfulapp.Awful.CopyURL")
+        .init("com.awfulapp.Awful.CopyURL")
     }
     
-    override class var activityCategory: UIActivity.Category {
-        return .action
-    }
-    
-    override var activityTitle: String? {
-        return overriddenTitle ?? LocalizedString("copy-url.title")
-    }
-    
-    override var activityImage: UIImage? {
-        return UIImage(named: "copy")
-    }
+    override class var activityCategory: UIActivity.Category { .action }
+    override var activityTitle: String? { LocalizedString("copy-url.title") }
+    override var activityImage: UIImage? { UIImage(named: "copy") }
     
     override func canPerform(withActivityItems activityItems: [Any]) -> Bool {
-        return activityItems.contains { $0 is Box }
+        activityItems.contains { $0 is Box }
     }
     
-    fileprivate var url: URL!
+    fileprivate var url: URL?
     
     override func prepare(withActivityItems activityItems: [Any]) {
-        url = activityItems.lazy.compactMap({ $0 as? Box }).first?.url
+        url = activityItems.lazy.compactMap { $0 as? Box }.first?.url
     }
     
     override func perform() {
