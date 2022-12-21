f82cc39cf52579e8a86ef536e0dc0e943fd6f8a8
commiekong <30882689+dfsm@users.noreply.github.com>
2022-12-21 14:06:32 +1100

    Using IntersectionObserver to detect displayed posts. Will use to fire twitter load script and delay those embed calls

--- App/Misc/HTMLRenderingHelpers.swift
+++ App/Misc/HTMLRenderingHelpers.swift
@@ -21,12 +21,21 @@ extension HTMLDocument {
             guard
                 pathComponents.count >= 4,
                 pathComponents[2].lowercased().hasPrefix("status")
-                else { continue }
+            else { continue }
             
             let id = pathComponents[3]
             guard id.unicodeScalars.allSatisfy(CharacterSet.decimalDigits.contains) else { continue }
             
+            
+//            let iframe = HTMLElement(tagName: "iframe", attributes: ["loading": "lazy"])
+//            let noscript = HTMLElement(tagName: "noscript", attributes: ["class": "loading-lazy"])
+//            noscript.addChild(iframe)
+  
             a["data-tweet-id"] = id
+            
+            a.toggleClass("")
+     //       a.parent?.addChild(noscript)
+     //       a.parent?.replace(child: a, with: noscript)
         }
     }
     
