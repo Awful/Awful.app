3363c1015d5a03fc62b6d06ff1d985a734cb0e37
Nolan Waite <nolan@nolanw.ca>
2023-04-09 00:40:06 -0300

    Fix some compile warnings in Xcode 14.3.

--- AwfulCore/Sources/AwfulCore/Model/PrivateMessage.swift
+++ AwfulCore/Sources/AwfulCore/Model/PrivateMessage.swift
@@ -2,7 +2,7 @@
 //
 //  Copyright 2014 Awful Contributors. CC BY-NC-SA 3.0 US https://github.com/Awful/Awful.app
 
-import Foundation
+import CoreData
 
 @objc(PrivateMessage)
 public class PrivateMessage: AwfulManagedObject, Managed {
