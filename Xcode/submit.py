8e44e2c8d1e7dafd0d2b68619d237a24d13d96fd
Nolan Waite <nolan@nolanw.ca>
2023-10-12 00:40:54 -0300

    Switch to notarytool for notarization.

--- Xcode/submit.py
+++ Xcode/submit.py
@@ -1,72 +1,46 @@
 # -*- coding: utf-8 -*-
 
+import json
 import os
 import plistlib
 from subprocess import CalledProcessError, check_call, check_output
 import sys
 import time
 
-def _get_notarization_info(request_uuid, apple_id_username):
-    return check_output(['xcrun', 'altool',
-                         '--notarization-info', request_uuid,
-                         '--output-format', 'xml',
-                         '--asc-provider', 'NolanWaite133631772',
-                         '--username', apple_id_username,
-                         '--password', '@keychain:Application Loader: {}'.format(apple_id_username),
-                        ])
-
-
-def _notarize(zip_path, apple_id_username):
+def _notarize(zip_path, apple_id, team_id):
     try:
-        return check_output(['xcrun', 'altool',
-                             '--notarize-app',
-                             '--primary-bundle-id', "com.awfulapp.Awful",
-                             '--output-format', 'xml',
-                             '--asc-provider', 'NolanWaite133631772',
-                             '--file', zip_path,
-                             '--username', apple_id_username,
-                             '--password', '@keychain:Application Loader: {}'.format(apple_id_username),
-                            ])
+        return check_call(['xcrun', 'notarytool',
+                           'submit',
+                           '--apple-id', apple_id,
+                           '--team-id', team_id,
+                           '--keychain-profile', 'Awful',
+                           '--wait',
+                            zip_path,
+                           ])
     except CalledProcessError as e:
         if e.returncode == 44:
             print(("To support scripted uploads: "
-                   "please launch Application Loader, "
-                   "sign in as '{}', "
-                   "and tick 'Keep me logged in'."
-                   "").format(apple_id_username),
+                   "obtain an app-specific password "
+                   "for the Apple ID '{}', "
+                   "and save it using: xcrun notarytool store-credentials Awful"
+                   "").format(apple_id),
                   file=sys.stderr)
         raise
 
 
-def notarize_and_staple(app_path, zip_path, apple_id_username):
-    upload_output = _notarize(zip_path, apple_id_username)
-    upload_plist = plistlib.loads(upload_output)
-
-    request_uuid = upload_plist['notarization-upload']['RequestUUID']
-    while True:
-        info_output = _get_notarization_info(request_uuid, apple_id_username)
-        info_plist = plistlib.loads(info_output)
-        status = info_plist['notarization-info']['Status']
-        if status == "in progress":
-            time.sleep(60)
-        else:
-            break
-
-    if status != "success":
-        print(info_output, file=sys.stderr)
-        raise Exception("Notarization failed")
-
+def notarize_and_staple(app_path, zip_path, apple_id, team_id):
+    _notarize(zip_path, apple_id, team_id)
     check_call(['xcrun', 'stapler', 'staple', app_path])
 
 
-def upload_to_app_store(ipa_path, apple_id_username):
+def upload_to_app_store(ipa_path, apple_id):
     try:
         check_call(['xcrun', 'altool',
                     '--upload-app',
                     '-f', ipa_path,
                     '-t', 'ios',
-                    '-u', apple_id_username,
-                    '-p', '@keychain:Application Loader: {}'.format(apple_id_username),
+                    '-u', apple_id,
+                    '-p', '@keychain:Application Loader: {}'.format(apple_id),
                    ])
     except CalledProcessError as e:
         if e.returncode == 44:
@@ -74,6 +48,6 @@ def upload_to_app_store(ipa_path, apple_id_username):
                    "please launch Application Loader, "
                    "sign in as '{}', "
                    "and tick 'Keep me logged in'."
-                   "").format(apple_id_username),
+                   "").format(apple_id),
                   file=sys.stderr)
         raise
