38e8b4d2504608c20d7a788ae1d69b08ec572352
Nolan Waite <nolan@nolanw.ca>
2024-03-03 03:19:03 -0400

    Build and commit a copy of lessc so we can do archive builds again (#1184)
    Apparently Xcode doesn't know how to build and use executables for build plugins when doing an archive?

--- LessStylesheet/Package.swift
+++ LessStylesheet/Package.swift
@@ -5,20 +5,22 @@ import PackageDescription
 let package = Package(
     name: "LessStylesheet",
     products: [
+        .executable(name: "lessc", targets: ["lessc"]),
         .plugin(name: "LessStylesheet", targets: ["LessStylesheet"]),
     ],
     targets: [
-        .plugin(
-            name: "LessStylesheet",
-            capability: .buildTool(),
-            dependencies: [
-                "lessc",
-            ]
-        ),
         .executableTarget(
             name: "lessc",
             resources: [
                 .copy("less.js"),
+            ]
+        ),
+        .plugin(
+            name: "LessStylesheet",
+            capability: .buildTool(),
+            dependencies: [
+                // No point; doesn't work in archive builds. See read me for more info.
+                // "lessc",
             ]
         ),
     ]
