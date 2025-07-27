29a44888d186e8b64657df86aaa7024e17229064
Jonathan Taylor <jon@jetaylor.net>
2025-07-27 14:12:02 -0500

    Fix bug where smiley picker wouldn't reload the theme

--- App/Composition/ShowSmilieKeyboardCommand.swift
+++ App/Composition/ShowSmilieKeyboardCommand.swift
@@ -69,6 +69,9 @@ final class ShowSmilieKeyboardCommand: NSObject {
         // Dismiss keyboard before showing smilie picker
         textView.resignFirstResponder()
         
+        // Get the current theme
+        let currentTheme = Theme.defaultTheme()
+        
         weak var weakTextView = textView
         let pickerView = SmiliePickerView(dataStore: smilieKeyboard.dataStore) { [weak self] smilieData in
             self?.insertSmilieData(smilieData)
@@ -85,6 +88,7 @@ final class ShowSmilieKeyboardCommand: NSObject {
                 weakTextView?.becomeFirstResponder()
             }
         }
+        .environment(\.theme, currentTheme)
         
         let hostingController = UIHostingController(rootView: pickerView)
         hostingController.modalPresentationStyle = .pageSheet
