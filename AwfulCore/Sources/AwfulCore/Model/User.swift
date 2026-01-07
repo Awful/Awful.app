3c6656402d9797475f05439a64bcc1b34086e728
commiekong <30882689+dfsm@users.noreply.github.com>
2026-01-07 12:41:53 +1100

    Fixed missing avatars in private messages caused by a duplicate empty dd.title tag

--- AwfulCore/Sources/AwfulCore/Model/User.swift
+++ AwfulCore/Sources/AwfulCore/Model/User.swift
@@ -63,9 +63,9 @@ extension User {
 // TODO: this is very stupid, just handle it during scraping
 public func extractAvatarURL(fromCustomTitleHTML customTitleHTML: String) -> URL? {
     let document = HTMLDocument(string: customTitleHTML)
-    let img = document.firstNode(matchingSelector: "div > img:first-child") ??
-        document.firstNode(matchingSelector: "body > img:first-child") ??
-        document.firstNode(matchingSelector: "a > img:first-child")
+    let img = document.firstNode(matchingSelector: "div > img:first-of-type") ??
+        document.firstNode(matchingSelector: "body > img:first-of-type") ??
+        document.firstNode(matchingSelector: "a > img:first-of-type")
 
     let src = img?["data-cfsrc"] ?? img?["src"]
     return src.flatMap { URL(string: $0) }
